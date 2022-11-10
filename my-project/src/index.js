e1c1099689ceeac31791f0e0deaf7a665792a2d2
marcelovenagui <marcelovenancioaguiar@gmail.com>
2022-11-10 20:47:52 -0300

    +

--- my-project/src/index.js
+++ my-project/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import './index.css'
 import ReactDOM from 'react-dom'
+import Primeiro from './components/primeiro'
 
 ReactDOM.render(
-    <h1>Olá React</h1>,
+    <Primeiro />,
     document.getElementById('root')
 )
