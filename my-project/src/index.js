7dcba180164fcd44eaf4a9db48df1f883be0b7a8
marcelovenagui <marcelovenancioaguiar@gmail.com>
2022-11-11 19:55:51 -0300

    props

--- my-project/src/index.js
+++ my-project/src/index.js
@@ -2,8 +2,12 @@ import React from 'react'
 import './index.css'
 import ReactDOM from 'react-dom'
 import Primeiro from './components/primeiro'
+import './components/ComParametro'
+import ComParametro from './components/ComParametro'
 
 ReactDOM.render(
-    <Primeiro />,
+    <div>
+        <Primeiro /> <ComParametro titulo='Esse é o título' subtitulo='Esse é o subtítulo'/>
+    </div>,
     document.getElementById('root')
 )
