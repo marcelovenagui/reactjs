5c04939890387be1604b6c68407b4b32f6a1e682
marcelovenagui <marcelovenancioaguiar@gmail.com>
2022-11-11 19:40:17 -0300

    Entendendo componentes

--- my-project/src/components/primeiro.jsx
+++ my-project/src/components/primeiro.jsx
@@ -1,6 +1,11 @@
+import React from 'react'
 
-function Primeiro(){
-    return 'Primeiro'
-}
 
-export default Primeiro
+export default () =>
+     <>
+        <h1>Primeiro componente (Arrow)</h1> 
+        <h2>Exemplo de um componente React</h2> 
+    </>
+
+
+
